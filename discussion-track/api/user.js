36d3c51f0ebed16a99937347e4beef6fa2963f89
aleoroeng <oro99eng@gmail.com>
2021-09-22 15:49:01 -0400

    added add_user endpoint

--- discussion-track/api/user.js
+++ discussion-track/api/user.js
@@ -1,21 +1,31 @@
-const user_db = require("./models/User_model");
+const user_db = require("./models/user_db");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 4000;
+user_model = {};
 
 app.post("/user", (req, res) => {
 	console.log(req.body);
-	res.send(req.body);
+
+	newUser = req.body;
+	user_db.add_user(user_model, newUser).then((added_user) => {
+		res.send(added_user);
+	});
 });
 
 app.get("/user", (req, res) => {});
 
 app.listen(PORT, () => {
-	user_db.setup().then((user_model) => {
-		console.log(user_model);
+	user_db.setup().then((model) => {
+		console.log(model);
+		user_model = model;
 	});
 	console.log(`Listening on port: ${PORT}`);
 });
